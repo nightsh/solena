b5371b3163704f1888a648bb2f5774fc6485bb5d
Ingo Malchow <imalchow@kde.org>
2012-04-12 14:47:45 +0200

    remove home link, we already have one

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -34,7 +34,6 @@
 				<div class="nav-pills pull-right nav-collapse">
 				<?php $this->widget('zii.widgets.CMenu',array(
 					'items'=>array(
-						array('label' => 'Home', 'url' => array('/site/index'), 'template' => '<i class="icon-home"></i>'),
 						array('label' => 'People', 'url' => array('/people/index'), 'template' => '<i class="icon-user"></i>{menu}'),
 						array('label' => 'Groups', 'url' => array('/groups/index'), 'template' => '<i class="icon-th-large"></i>{menu}'),
 						array('label' => 'Registrations', 'url' => array('/registration/list'), 'template' => '<i class="icon-pencil"></i>{menu}', 'visible' => Yii::app()->user->checkAccess('sysadmins')),
