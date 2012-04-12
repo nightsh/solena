8cc8ba65311a4eeeb8c5c14f362a7f7d94e570e2
Ingo Malchow <imalchow@kde.org>
2012-04-12 12:39:04 +0200

    add class to menu

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -43,7 +43,8 @@
 						array('label' => 'Login', 'url'=>array('/site/login'), 'visible' => Yii::app()->user->isGuest),
 						array('label' => 'Register', 'url'=>array('/registration/index'), 'visible' => Yii::app()->user->isGuest),
 						array('label' => 'Logout ('.Yii::app()->user->name.')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
-						),
+					),
+					'htmlOptions' => array('class'=>'nav'),
 					)); ?>
 				</div>
 			</div>
