4de6af94e7fa2c34637c314ec4447845ddad8531
Ingo Malchow <imalchow@kde.org>
2012-04-12 21:20:06 +0200

    adjust 2 column pages

--- protected/views/layouts/column2.php
+++ protected/views/layouts/column2.php
@@ -1,28 +1,26 @@
 <?php $this->beginContent('//layouts/main'); ?>
-<div class="container">
-	<div class="span-19">
-		<div id="content">
-			<?php
-				foreach(Yii::app()->user->getFlashes() as $key => $message) {
-					echo CHtml::tag('div', array('class' => 'flash-' . $key), CHtml::encode($message));
-				}
-			?>
-			<?php echo $content; ?>
-		</div><!-- content -->
-	</div>
-	<div class="span-5 last">
-		<div id="sidebar">
+<div id="page" class="span-7">
+	<div id="content">
 		<?php
-			$this->beginWidget('zii.widgets.CPortlet', array(
-				'title'=>'Operations',
-			));
-			$this->widget('zii.widgets.CMenu', array(
-				'items'=>$this->menu,
-				'htmlOptions'=>array('class'=>'operations'),
-			));
-			$this->endWidget();
+			foreach(Yii::app()->user->getFlashes() as $key => $message) {
+				echo CHtml::tag('div', array('class' => 'flash-' . $key), CHtml::encode($message));
+			}
 		?>
-		</div><!-- sidebar -->
+		<?php echo $content; ?>
+	</div><!-- content -->
 	</div>
+<div class="span-4">
+	<div id="sidebar">
+	<?php
+		$this->beginWidget('zii.widgets.CPortlet', array(
+			'title'=>'Operations',
+		));
+		$this->widget('zii.widgets.CMenu', array(
+			'items'=>$this->menu,
+			'htmlOptions'=>array('class'=>'operations'),
+		));
+		$this->endWidget();
+	?>
+	</div><!-- sidebar -->
 </div>
 <?php $this->endContent(); ?>
