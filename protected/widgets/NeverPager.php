30a638bdbf2dd743ce7004cdc772e6a660e69c89
Ingo Malchow <imalchow@kde.org>
2012-04-14 13:24:14 +0200

    add new pagination widget

---
+++ protected/widgets/NeverPager.php
@@ -0,0 +1,12 @@
+<?php
+/*********************************************************
+ *  Pagination bootstrapified
+ ********************************************************/
+
+class NeverPager extends CLinkPager
+{
+	/**
+	 * @var array HTML attributes for the pager container tag.
+	 */
+	public $htmlOptions=array('class' => 'pagination');
+}
