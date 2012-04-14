c94380bf7bcaa4b53cc291313b7b2a7a3ea3eed7
Ingo Malchow <imalchow@kde.org>
2012-04-14 12:20:36 +0200

    rearrange avatar page

--- protected/views/people/editAvatar.php
+++ protected/views/people/editAvatar.php
@@ -18,21 +18,19 @@ $this->menu = $this->generateMenu($model);
 	'htmlOptions' => array('enctype' => 'multipart/form-data'),
 )); ?>
 
-	<div class="row">
-		<?php echo CHtml::image( CHtml::normalizeUrl(array('viewAvatar', 'uid' => $model->uid)) ); ?>
-	</div>
-
-	<?php if( isset($model->jpegPhoto) && !$model->jpegPhoto instanceof CUploadedFile ) { ?>
-	<div class="row">
-		<?php echo CHtml::submitButton('Clear avatar', array('name' => 'clearAvatar', 'class' => 'btn')); ?>
-	</div>
-	<?php } ?>
-
-	<div class="row">
+	<div class="span8">
 		<?php echo $form->labelEx($model, 'jpegPhoto'); ?>
 		<?php echo $form->fileField($model, 'jpegPhoto'); ?>
 		<?php echo $form->error($model, 'jpegPhoto'); ?>
 	</div>
+	<div class="span2">
+		<?php echo CHtml::image( CHtml::normalizeUrl(array('viewAvatar', 'uid' => $model->uid)), array('class' => 'thumbnail') ); ?>
+		<?php if( isset($model->jpegPhoto) && !$model->jpegPhoto instanceof CUploadedFile ) { ?>
+			<?php echo CHtml::submitButton('Clear avatar', array('name' => 'clearAvatar', 'class' => 'btn')); ?>
+		<?php } ?>
+	</div>
+
+	
 	<div class="row buttons">
 		<?php echo CHtml::submitButton('Upload', array('name' => 'uploadAvatar', 'class' => 'btn btn-primary')); ?>
 	</div>
