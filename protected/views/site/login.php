2ca7b0c479e686e9c49dad6a0d014142d6e25082
Ben Cooksley <bcooksley@kde.org>
2012-04-16 00:31:58 +1200

    One more

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -34,7 +34,7 @@ $this->breadcrumbs=array(
 
 	<div class="row buttons">
 		<?php echo CHtml::submitButton('Login', array('class' => 'btn btn-primary')); ?>
-		<?php echo CHtml::link('Reset password', array('passwordReset')); ?>
+		<?php echo CHtml::link('Reset Password', array('passwordReset')); ?>
 	</div>
 
 <?php $this->endWidget(); ?>
