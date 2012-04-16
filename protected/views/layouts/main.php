36f5496555a2ed8ce4987834cb419fb1147f909b
Ingo Malchow <imalchow@kde.org>
2012-04-16 11:52:41 +0200

    adjust css include for CDN

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -8,8 +8,8 @@
 	<meta name="author" content="Ben Cooksley">
 
 	<!-- Le styles -->
-	<link href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap.css" rel="stylesheet">
-	<link href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap-solena.css" rel="stylesheet">
+	<link href="http://files.kde.org/neverland/css/bootstrap.css" rel="stylesheet">
+	<link href="http://files.kde.org/neverland/css/bootstrap-solena.css" rel="stylesheet">
 	
 	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
 	<!--[if lt IE 9]>
