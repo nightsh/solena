bbafaa7447d50b4498f88fdf5493427545bd93d9
Ben Cooksley <bcooksley@kde.org>
2012-04-21 00:55:42 +1200

    Fix the multiline editor adding

--- js/multiedit-widget.js
+++ js/multiedit-widget.js
@@ -1,7 +1,7 @@
 function multiEditAddInput( addButton ) {
 	var container = addButton.siblings('div');
 	var clone = container.children().last().clone();
-	var editor = clone.children('input').first();
+	var editor = clone.children().first();
 	var entryNumber = parseInt(editor.attr('id').match(/\d+/)) + 1
 	var id = editor.attr('id').replace(/_\d+$/, '_' + entryNumber);
 	var name = editor.attr('name').replace(/\[\d+\]$/, '[' + entryNumber + ']');
