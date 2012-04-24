28f018f08bbc825321259190086051dd04884c11
Ingo Malchow <imalchow@kde.org>
2012-04-24 12:23:33 +0200

    use yii internal tag rendering

--- protected/widgets/NBreadcrumbs.php
+++ protected/widgets/NBreadcrumbs.php
@@ -26,8 +26,7 @@ class NBreadcrumbs extends CBreadcrumbs {
 			echo CHtml::openTag($this->tagName,$this->htmlOptions)."\n";
 			echo CHtml::openTag('li\n');
 			echo CHtml::openTag('a href="'.Yii::app()->homeUrl.'"');
-			echo CHtml::openTag('i class="icon-home icon-white');
-			echo CHtml::closeTag('i');
+			echo CHtml::tag('i', array( 'class' => 'icon-home icon-white'), false, true);
 			echo "Home";
 			echo CHtml::closeTag('a');
 			echo CHtml::closeTag('li');
