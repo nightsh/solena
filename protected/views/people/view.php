50b9a446ead3dc832b6f4dc15441615e3e4e58a9
Ingo Malchow <imalchow@kde.org>
2012-04-27 23:31:31 +0200

    people view needs a bit more nesting

--- protected/views/people/view.php
+++ protected/views/people/view.php
@@ -33,44 +33,48 @@ $this->menu = $this->generateMenu($model);
 		<?php echo CHtml::image( CHtml::normalizeUrl(array('viewAvatar', 'uid' => $model->uid)), '',array('class' => 'thumbnail pull-right') ); ?>
 	</div>
 
-	<div class="row">
-		<h3>Contact Information</h3>
+	<div class="row-fluid">
+		<div class="span12">
+			<h3>Contact Information</h3>
 
-		<?php $this->widget('zii.widgets.CDetailView', array(
-			'data' => $model,
-			'attributes' => array(
-				'homePostalAddress:ntext',
-				'homePhone',
-				'labeledURI:url',
-				'ircNick',
-				'jabberID',
-				'emailAddresses:email',
-				array(
-					'name' => 'evMail',
-					'type' => 'email',
-					'visible' => isset($model->evMail),
+			<?php $this->widget('zii.widgets.CDetailView', array(
+				'data' => $model,
+				'attributes' => array(
+					'homePostalAddress:ntext',
+					'homePhone',
+					'labeledURI:url',
+					'ircNick',
+					'jabberID',
+					'emailAddresses:email',
+					array(
+						'name' => 'evMail',
+						'type' => 'email',
+						'visible' => isset($model->evMail),
+					),
 				),
-			),
-			'htmlOptions' => array('class' => 'table table-bordered table-striped'),
-		)); ?>
+				'htmlOptions' => array('class' => 'table table-bordered table-striped'),
+			)); ?>
+		</div>
 	</div>
 
-	<div class="row">
-		<h3>Member of Groups</h3>
+	<div class="row-fluid">
+		<div class="span12">
+			<h3>Member of Groups</h3>
 
-		<?php $this->widget('application.components.NeverGridView', array(
-			'id'=> 'group-grid',
-			'dataProvider'=> $groupData,
-			'template' => '{items}{pager}',
-			'columns'=> array(
-				array(
-					'class' => 'LinkDataColumn',
-					'name' => 'cn',
-					'header' => 'Group Name',
-					'urlExpression' => 'Yii::app()->createUrl("/groups/view", array("cn" => $data->cn))',
+			<?php $this->widget('application.components.NeverGridView', array(
+				'id'=> 'group-grid',
+				'dataProvider'=> $groupData,
+				'template' => '{items}{pager}',
+				'columns'=> array(
+					array(
+						'class' => 'LinkDataColumn',
+						'name' => 'cn',
+						'header' => 'Group Name',
+						'urlExpression' => 'Yii::app()->createUrl("/groups/view", array("cn" => $data->cn))',
+					),
+					'description:text:Description',
 				),
-				'description:text:Description',
-			),
-		)); ?>
+			)); ?>
+		</div>
 	</div>
 </div>
