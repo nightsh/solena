382b8f33ad05a8a6540e60d35d8c7626a445c914
Ingo Malchow <imalchow@kde.org>
2012-04-27 16:37:54 +0200

    fix wrong list nesting

--- protected/widgets/NBreadcrumbs.php
+++ protected/widgets/NBreadcrumbs.php
@@ -38,13 +38,13 @@ class NBreadcrumbs extends CBreadcrumbs {
 		if($this->homeLink===null)
 			$links[]='<li>'.CHtml::tag('i', array( 'class' => 'icon-home icon-white'), ' ', true).CHtml::link(Yii::t('zii','Home'),Yii::app()->homeUrl).'</li>';
 		else if($this->homeLink!==false)
-			$links[]='<li>'.$this->homeLink.'<li class="divider-vertical"></li></li>';
+			$links[]='<li>'.$this->homeLink.'</li><li class="divider-vertical"></li>';
 		foreach($this->links as $label=>$url)
 		{
 			if(is_string($label) || is_array($url))
-				$links[]=CHtml::link($this->encodeLabel ? '<li><li class="divider-vertical"></li>'.CHtml::encode($label).'</li>' :'<li>'. $label, $url.'</li>');
+				$links[]=CHtml::link($this->encodeLabel ? '<li class="divider-vertical"></li><li>'.CHtml::encode($label).'</li>' :'<li>'. $label, $url.'</li>');
 			else
-				$links[]=($this->encodeLabel ? '<li><li class="divider-vertical"></li>'.CHtml::encode($url).'</li>' : '<li>'.$url.'</li>');
+				$links[]=($this->encodeLabel ? '<li class="divider-vertical"></li><li>'.CHtml::encode($url).'</li>' : '<li>'.$url.'</li>');
 		}
 		echo implode($links);
 		echo CHtml::closeTag($this->tagName);
