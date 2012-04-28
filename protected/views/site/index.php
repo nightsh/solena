5b8449a3f7ee08072d07d606049f6f064fac3567
Ingo Malchow <imalchow@kde.org>
2012-04-28 14:19:09 +0200

    change frontpage

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,8 +1,9 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 <div class="hero-unit">
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+	<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+	<p class="lead">The central place for Accounts and Account data</p>
+</div>
 
 <p>You may change the content of this page by modifying the following two files:</p>
 <ul>
@@ -14,4 +15,3 @@
 the <a href="http://www.yiiframework.com/doc/">documentation</a>.
 Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
 should you have any questions.</p>
-</div>
