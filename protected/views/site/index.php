4e2997ecc75e31ca835f932034ff3ed40f3ecd4c
Ingo Malchow <imalchow@kde.org>
2012-04-30 23:27:03 +0200

    add iko supported sites as list

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -5,6 +5,31 @@
 	<p class="lead">The central place for Accounts and Account data</p>
 </div>
 
-<p>Here you may sign up and instantly get access to various KDE sites via a SSO (Single Sign-On) mechanism</p>
+<p>
+Here you may sign up and instantly get access to various KDE sites via a SSO (Single Sign-On) mechanism.
+</p>
 
 <p>You can also change your Account settings and personal details which can be used on other KDE sites</p>
+
+<div>KDE Sites that are already supporting a KDE Identity login are (in no particular order):
+	<ul>
+		<li>
+		   <a href="http://forum.kde.org">Forums</a>
+		</li>
+		<li>
+		  <a href="http://techbase.kde.org">Techbase Wiki</a>
+		</li>
+		<li>
+		  <a href="http://userbase.kde.org">Userbase Wiki</a>
+		</li>
+		<li>
+		  <a href="http://community.kde.org">Community Wiki</a>
+		</li>
+		<li>
+		  <a href="http://bugs.kde.org">KDE Bugs</a>
+		</li>
+		<li>
+		  <a href="http://sprints.kde.org">Sprint Planning</a>
+		</li>
+	</ul>
+</div>
