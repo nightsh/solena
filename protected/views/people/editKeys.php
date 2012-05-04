1a501221a34ecc754348a370ca3ee52633fa38ce
Ingo Malchow <imalchow@kde.org>
2012-05-04 09:41:17 +0200

    remove borders

--- protected/views/people/editKeys.php
+++ protected/views/people/editKeys.php
@@ -35,6 +35,7 @@ Yii::app()->clientScript->registerScript("disableCheckAll", "$('input.select-on-
 				'fingerprint:text:Fingerprint',
 				'comment:text:Comment',
 			),
+			'itemsCssClass' => 'table table-striped table-condensed',
 		)); ?>
 		<?php
 			if( $dataProvider->itemCount > 0 ) {
