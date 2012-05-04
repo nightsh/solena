e97d5a8d947ab692718733c4f5e01d883ef7d067
Ben Cooksley <bcooksley@kde.org>
2012-05-05 10:42:43 +1200

    Update front page

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -2,34 +2,10 @@
 <div class="hero-unit">
 	<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-	<p class="lead">The central place for Accounts and Account data</p>
-</div>
-
-<p>
-Here you may sign up and instantly get access to various KDE sites via a SSO (Single Sign-On) mechanism.
-</p>
-
-<p>You can also change your Account settings and personal details which can be used on other KDE sites</p>
-
-<div>KDE Sites that are already supporting a KDE Identity login are (in no particular order):
-	<ul>
-		<li>
-		   <a href="http://forum.kde.org">Forums</a>
-		</li>
-		<li>
-		  <a href="http://techbase.kde.org">Techbase Wiki</a>
-		</li>
-		<li>
-		  <a href="http://userbase.kde.org">Userbase Wiki</a>
-		</li>
-		<li>
-		  <a href="http://community.kde.org">Community Wiki</a>
-		</li>
-		<li>
-		  <a href="http://bugs.kde.org">KDE Bugs</a>
-		</li>
-		<li>
-		  <a href="http://sprints.kde.org">Sprint Planning</a>
-		</li>
-	</ul>
+	<p class="lead">Welcome to the home of the KDE Identity system. </p>
+	<p>
+		KDE Identity is the central account manager for the KDE.org infrastructure.
+		<br />
+		A KDE Identity account allows unified access to most KDE.org websites, and commit access to the various code repositories.
+	</p>
 </div>
