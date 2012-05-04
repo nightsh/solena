8b9a6568bb8dcd541f74bb14b4565a37f388b98d
Ingo Malchow <imalchow@kde.org>
2012-05-04 09:29:17 +0200

    give it another try

--- protected/views/people/editAvatar.php
+++ protected/views/people/editAvatar.php
@@ -16,28 +16,29 @@ $this->menu = $this->generateMenu($model);
 			<?php $form = $this->beginWidget('CActiveForm', array(
 				'id' => 'person-form',
 				'enableAjaxValidation' => false,
-				'htmlOptions' => array('enctype' => 'multipart/form-data', 'class' => 'row-fluid'),
+				'htmlOptions' => array('enctype' => 'multipart/form-data'),
 			)); ?>
-
-			<div class="span9">
-				<div class="row-fluid">
-					<?php echo $form->labelEx($model, 'jpegPhoto'); ?>
-					<?php echo $form->error($model, 'jpegPhoto'); ?>
-					<?php echo $form->fileField($model, 'jpegPhoto'); ?>
+			<div class="row-fluid">
+				<div class="span9">
+					<div class="row-fluid">
+						<?php echo $form->labelEx($model, 'jpegPhoto'); ?>
+						<?php echo $form->error($model, 'jpegPhoto'); ?>
+						<?php echo $form->fileField($model, 'jpegPhoto'); ?>
+					</div>
+					<div class="row-fluid buttons">
+						<?php echo CHtml::submitButton('Upload Avatar', array('name' => 'uploadAvatar', 'class' => 'btn btn-primary')); ?>
+					</div>
 				</div>
-				<div class="row-fluid buttons">
-					<?php echo CHtml::submitButton('Upload Avatar', array('name' => 'uploadAvatar', 'class' => 'btn btn-primary')); ?>
+				<div class="span3">
+					<?php echo CHtml::image( CHtml::normalizeUrl(array('viewAvatar', 'uid' => $model->uid)), '', array('class' => 'thumbnail') ); ?>
+					<br />
+					<?php if( isset($model->jpegPhoto) && !$model->jpegPhoto instanceof CUploadedFile ) { ?>
+						<?php echo CHtml::submitButton('Clear Avatar', array('name' => 'clearAvatar', 'class' => 'btn')); ?>
+					<?php } ?>
 				</div>
+				<div class="clearfix"></div>
+				<?php $this->endWidget(); ?>
 			</div>
-			<div class="span3">
-				<?php echo CHtml::image( CHtml::normalizeUrl(array('viewAvatar', 'uid' => $model->uid)), '', array('class' => 'thumbnail') ); ?>
-				<br />
-				<?php if( isset($model->jpegPhoto) && !$model->jpegPhoto instanceof CUploadedFile ) { ?>
-					<?php echo CHtml::submitButton('Clear Avatar', array('name' => 'clearAvatar', 'class' => 'btn')); ?>
-				<?php } ?>
-			</div>
-			<div class="clearfix"></div>
-			<?php $this->endWidget(); ?>
 		</div>
 	</div>
 </div>
