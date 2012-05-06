63cf36c8846a99d60156bbfe8ac8d454c252e231
Ben Cooksley <bcooksley@kde.org>
2012-05-07 09:29:49 +1200

    Add a link to the wiki page for people to find out more about getting developer access

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -46,6 +46,7 @@
 						array('label' => 'Groups', 'url' => array('/groups/index'), 'glyphIcon' => 'th-list'),
 						array('label' => 'Registrations', 'url' => array('/registration/list'), 'glyphIcon' => 'pencil', 'visible' => Yii::app()->user->checkAccess('sysadmins')),
 						array('label' => 'Developer Applications', 'url' => array('/developerApplication/list'), 'glyphIcon' => 'file', 'visible' => Yii::app()->user->checkAccess('sysadmins')),
+						array('label' => 'Apply for Developer Access', 'url' => 'http://techbase.kde.org/Contribute/Get_a_Contributor_Account', 'glyphIcon' => 'file', 'visible' => Yii::app()->user->checkAccess('users')),
 						array('label' => 'My Account', 'url' => array('/people/view', 'uid' => Yii::app()->user->id), 'glyphIcon' => 'user', 'visible' => !Yii::app()->user->isGuest),
 						array('label' => 'Login', 'url'=>array('/site/login'), 'glyphIcon' => 'cog', 'visible' => Yii::app()->user->isGuest),
 						array('label' => 'Register', 'url'=>array('/registration/index'), 'glyphIcon' => 'pencil', 'visible' => Yii::app()->user->isGuest),
